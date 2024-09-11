6d2e056ce0ef0ebf23307c04cfb8d5e1db7ba013
Shaista <92063857+shaista201@users.noreply.github.com>
2024-09-11 22:29:06 +0530

    Update root.c

--- arrays/root.c
+++ arrays/root.c
@@ -1,4 +1,6 @@
-/*2. Write a program that prompts the user for a positive integer and then reports the closest integer having a whole number square root. For example, if the user enters 8, then the program reports 9. If the user enters 18, then the program reports 16.The program should work  for any number having one to seven digits.*/
+/*2. Write a program that prompts the user for a positive integer and then reports the closest integer having a whole number square root. 
+For example, if the user enters 8, then the program reports 9. If the user enters 18, then the program reports 16.
+The program should work  for any number having one to seven digits.*/
 #include<stdio.h>
 #include<stdlib.h>
 
